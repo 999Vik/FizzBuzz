8331ba926c7095f64d63832e9d0bf47fb4c51f3b
Vikram Makkar <72366705+999Vik@users.noreply.github.com>
2023-09-11 18:50:18 -0400

    add Multiple file

--- src/Multiple.java
+++ src/Multiple.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class Multiple {
+public class Multiple {
+    public static void main(String[] args) {
+        int count = 0;
+        for (int i=0; i < 1000; i++){
+            boolean multiple3 = i%3 == 0;
+            boolean multiple5 = i%5 == 0;
+
+            if (multiple3 || multiple5){
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
 }
