0dea227d0af7bbbe382f91b419e959a4d7757cbc
Vikram Makkar <72366705+999Vik@users.noreply.github.com>
2023-09-11 18:12:07 -0400

    Main File

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        FizzBuzz.fizzBuzz(5);
+        String[] argsMain =  new String[5];
+        FizzBuzz.main(argsMain);
+    }
+}
